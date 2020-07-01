2db837e960d2e216af4d740251a2428d5830be7e
Daniel Fuqua <fuquardwork@gmail.com>
2020-07-01 15:46:59 -0500

    full crud post fixed

---
+++ Gifter/Controllers/UserProfileController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gifter.Data;
+using Gifter.Repositories;
+using Gifter.Models;
+
+namespace Gifter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly UserProfileRepository _UserProfileRepository;
+        public UserProfileController(ApplicationDbContext context)
+        {
+            _UserProfileRepository = new UserProfileRepository(context);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_UserProfileRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var userProfile = _UserProfileRepository.GetById(id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return Ok(userProfile);
+        }
+
+        //[HttpGet("getbyuser/{id}")]
+        //public IActionResult GetByUser(int id)
+        //{
+        //    return Ok(_postRepository.GetByUserProfileId(id));
+        //}
+
+        //[HttpPost]
+        //public IActionResult Post(Post post)
+        //{
+        //    _postRepository.Add(post);
+        //    return CreatedAtAction("Get", new { id = post.Id }, post);
+        //}
+
+        //[HttpPut("{id}")]
+        //public IActionResult Put(int id, Post post)
+        //{
+        //    if (id != post.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _postRepository.Update(post);
+        //    return NoContent();
+        //}
+
+        //[HttpDelete("{id}")]
+        //public IActionResult Delete(int id)
+        //{
+        //    _postRepository.Delete(id);
+        //    return NoContent();
+        //}
+
+    }
+}
--- Gifter/Repositories/UserProfileRepository.cs
+++ Gifter/Repositories/UserProfileRepository.cs
@@ -1,11 +1,56 @@
-﻿using System;
+﻿using System.Linq;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Gifter.Data;
+using Gifter.Models;
 
 namespace Gifter.Repositories
 {
     public class UserProfileRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public UserProfileRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<UserProfile> GetAll()
+        {
+            return _context.UserProfile.ToList();
+        }
+
+        public UserProfile GetById(int id)
+        {
+            return _context.UserProfile.FirstOrDefault(uP => uP.Id == id);
+        }
+
+        //public List<Post> GetByUserProfileId(int id)
+        //{
+        //    return _context.Post.Include(p => p.UserProfile)
+        //                    .Where(p => p.UserProfileId == id)
+        //                    .OrderBy(p => p.Title)
+        //                    .ToList();
+        //}
+
+        //public void Add(Post post)
+        //{
+        //    _context.Add(post);
+        //    _context.SaveChanges();
+        //}
+
+        //public void Update(Post post)
+        //{
+        //    _context.Entry(post).State = EntityState.Modified;
+        //    _context.SaveChanges();
+        //}
+
+        //public void Delete(int id)
+        //{
+        //    var post = GetById(id);
+        //    _context.Post.Remove(post);
+        //    _context.SaveChanges();
+        //}
+
     }
 }
