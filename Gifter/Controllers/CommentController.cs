264f4e5bd54f0d1b4bc62ec1bd683fa96f22a231
Daniel Fuqua <fuquardwork@gmail.com>
2020-07-02 10:55:32 -0500

    getall, getbyid, getbypost

---
+++ Gifter/Controllers/CommentController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gifter.Data;
+using Gifter.Repositories;
+using Gifter.Models;
+
+namespace Gifter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly CommentRepository _commentRepository;
+        public CommentController(ApplicationDbContext context)
+        {
+            _commentRepository = new CommentRepository(context);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_commentRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var comment = _commentRepository.GetById(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            return Ok(comment);
+        }
+
+        [HttpGet("getbypost/{id}")]
+        public IActionResult GetByPost(int id)
+        {
+            return Ok(_commentRepository.GetByPostId(id));
+        }
+
+        //[HttpPost]
+        //public IActionResult Post(Post post)
+        //{
+        //    _postRepository.Add(post);
+        //    return CreatedAtAction("Get", new { id = post.Id }, post);
+        //}
+
+        //[HttpPut("{id}")]
+        //public IActionResult Put(int id, Post post)
+        //{
+        //    if (id != post.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _postRepository.Update(post);
+        //    return NoContent();
+        //}
+
+        //[HttpDelete("{id}")]
+        //public IActionResult Delete(int id)
+        //{
+        //    _postRepository.Delete(id);
+        //    return NoContent();
+        //}
+
+    }
+}
---
+++ Gifter/Repositories/CommentRepository.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Gifter.Data;
+using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public class CommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Comment> GetAll()
+        {
+            return _context.Comment.ToList();
+        }
+
+        public Comment GetById(int id)
+        {
+            return _context.Comment.FirstOrDefault(c => c.Id == id);
+        }
+
+        public List<Comment> GetByPostId(int id)
+        {
+            return _context.Comment
+                            .Where(c => c.PostId == id)
+                            .ToList();
+        }
+
+        //public void Add(Post post)
+        //{
+        //    _context.Add(post);
+        //    _context.SaveChanges();
+        //}
+
+        //public void Update(Post post)
+        //{
+        //    _context.Entry(post).State = EntityState.Modified;
+        //    _context.SaveChanges();
+        //}
+
+        //public void Delete(int id)
+        //{
+        //    var post = GetById(id);
+        //    _context.Post.Remove(post);
+        //    _context.SaveChanges();
+        //}
+
+    }
+}
